96c9ee18d0600ca0a7d45c0efa597ad02b8615bc
nancyho629 <nancyho629@gmail.com>
2019-11-25 13:33:49 -0500

    Clean up header
    -Removed Home button since users can enter a subreddit or leave it blank
    and top posts from specified subreddit or all will be returned,
    respectively

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,5 @@
-import React, { Fragment } from 'react'
-import Nav from 'react-bootstrap/Nav'
+import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-
-const alwaysOptions = (
-  <Fragment>
-    <Nav.Link to="/">Home</Nav.Link>
-  </Fragment>
-)
 
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
@@ -15,9 +8,6 @@ const Header = ({ user }) => (
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
-        { alwaysOptions }
-      </Nav>
     </Navbar.Collapse>
   </Navbar>
 )
