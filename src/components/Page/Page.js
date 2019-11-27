c91691bdd0af2196c04306919dacc6b9b95bf340
nancyho629 <nancyho629@gmail.com>
2019-11-27 09:39:57 -0500

    Add upvote data
    -Tried to show author or subreddit name of the post but would freeze the
     page
    -Settled on showing number of upvotes in the mean time in the expansion
    panel

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,7 +4,7 @@ import Post from '../Post/Post'
 class Page extends Component {
   render () {
     return this.props.page.map((post) => (
-      <Post key={post.id} id={post.id} title={post.title} thumbnail={post.thumbnail} url={post.url} />
+      <Post key={post.id} id={post.id} title={post.title} thumbnail={post.thumbnail} url={post.url} ups={post.ups} />
     ))
   }
 }
