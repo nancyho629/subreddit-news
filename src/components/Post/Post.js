500a1fde609f051286eeacb40bf8311d43d80f16
nancyho629 <nancyho629@gmail.com>
2019-11-27 09:41:55 -0500

    Style with Material UI expansion panel
    -To show more information on the post
    -Currently displays number of upvotes, future version to display author
    of the post, subreddit the post is from, and comments/replies

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 class Post extends Component {
   render () {
     return (
       <div>
-        <a target="_blank" rel="noopener noreferrer" href={ this.props.url} >{this.props.title} </a>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography><a target="_blank" rel="noopener noreferrer" href={ this.props.url} >{this.props.title} </a></Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Upvotes: { this.props.ups }
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </div>
     )
   }
